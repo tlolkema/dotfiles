38f1ab8fc7f90d2fc938a455bea7347cc6b99f03
Tim Lolkema <timlolkema@gmail.com>
2023-02-25 19:11:29 +0100

    add vi alias to nvim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,11 @@ DOTFILES=~/dotfiles
 
 # Aliasses
 alias activate="source env/bin/activate"
+alias sourcezsh="source $DOTFILES/zsh/.zshrc"
 alias zshconfig="vim $DOTFILES/zsh/.zshrc"
 alias cl=clear
 alias vim=nvim
+alias vi=nvim
 alias lg=lazygit
 alias cat=bat
 alias nvm=fnm
