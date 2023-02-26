3d24802dbdb8917e9d2b1e8347803193d5498887
Tim Lolkema <timlolkema@gmail.com>
2023-02-26 12:42:20 +0100

    add note zsh function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,3 +6,12 @@ ai() {
         sh "$DOTFILES/scripts/openai.sh" "$1"
     fi
 }
+
+NOTES_DIR=~/Documents/Notes
+
+note() {
+    if [ -n "$1" ]
+    then
+        printf "- $1\n\n" >> "$NOTES_DIR/notes.md" 
+    fi
+}
