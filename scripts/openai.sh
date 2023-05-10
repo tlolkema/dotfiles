ece1466f0d13293e2f3de72d6e6b9770a29c1991
Tim Lolkema <timlolkema@Tims-MacBook-Pro.local>
2023-05-10 09:44:47 +0200

    change cut to sed

--- scripts/openai.sh
+++ scripts/openai.sh
@@ -16,7 +16,7 @@ response=$(curl -s https://api.openai.com/v1/chat/completions \
     "messages": [{"role": "user", "content": "'"$clean_input"'"}]
   }')
 
-generated_text=$(printf '%s' "$response" | jq '.choices[0].message.content' | cut -c 1-)
+generated_text=$(printf '%s' "$response" | jq '.choices[0].message.content' | sed -e 's/^"//' -e 's/"$//')
 
 printf "$generated_text\n"
 printf "$generated_text\n" | pbcopy
