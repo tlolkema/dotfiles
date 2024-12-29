fb7733fa313d10e8a02a530fcaf535a4d3d74d45
Tim Lolkema <timlolkema@tims-mbp.kpn>
2024-12-29 22:29:41 +0100

    cursor

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,8 @@ DOTFILES=~/dotfiles
 alias activate="source env/bin/activate"
 alias sourcezsh="source $DOTFILES/zsh/.zshrc"
 alias zshconfig="vim $DOTFILES/zsh/.zshrc"
+alias code="open -a /Applications/Cursor.app"
+
 alias cl=clear
 alias lg=lazygit
 alias cat=bat
