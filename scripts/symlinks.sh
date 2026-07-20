356912d6b680da0f32a7e9b32da1108303a535c8
Tim Lolkema <mail@timlolkema.nl>
2026-07-20 15:30:29 +0200

    chore: configure herdr keybindings

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -6,10 +6,12 @@ rm ~/.gitconfig
 rm ~/.zshrc
 rm ~/.vimrc
 rm ~/.config/ghostty/config
+rm ~/.config/herdr/config.toml
 
 # Symlinks to dotfiles
 ln -fs ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -fs ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -fs ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -fs ~/dotfiles/ghostty/config ~/.config/ghostty/config
+ln -fs ~/dotfiles/herdr/config.toml ~/.config/herdr/config.toml
 ln -fs ~/dotfiles/aerospace/.aerospace.toml ~/.aerospace.toml
