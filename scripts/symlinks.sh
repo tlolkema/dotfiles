708cd644169b0226899170bf14671ee50e2de32d
Tim Lolkema <mail@timlolkema.nl>
2026-07-20 15:34:23 +0200

    chore: configure hunk theme

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -7,6 +7,7 @@ rm ~/.zshrc
 rm ~/.vimrc
 rm ~/.config/ghostty/config
 rm ~/.config/herdr/config.toml
+rm ~/.config/hunk/config.toml
 
 # Symlinks to dotfiles
 ln -fs ~/dotfiles/vim/.vimrc ~/.vimrc
@@ -14,4 +15,5 @@ ln -fs ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -fs ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -fs ~/dotfiles/ghostty/config ~/.config/ghostty/config
 ln -fs ~/dotfiles/herdr/config.toml ~/.config/herdr/config.toml
+ln -fs ~/dotfiles/hunk/config.toml ~/.config/hunk/config.toml
 ln -fs ~/dotfiles/aerospace/.aerospace.toml ~/.aerospace.toml
